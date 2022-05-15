9624623625ced2b752eb189595ea6bb887297510
Kevin Damian Aceves Siordia <kevin.aceves@hotmail.com>
2022-05-15 16:39:25 +0200

    Requirements basic, file template

---
+++ bot.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import numpy as np
+import random
+import win32api, win32con
+
+if __name__ == "__main__":
+    
